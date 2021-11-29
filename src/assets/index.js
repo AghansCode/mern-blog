8df21c3f2fa9164615193074469f46dbb81a7f61
AghansCode <ganis.ade23@gmail.com>
2021-11-29 21:19:15 +0700

    membuat styling mainapp, header, dan footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,13 @@
 import RegisterBg from "./images/register-bg.jpg";
 import LoginBg from "./images/login-bg.jpg";
+import LogoJS from "./images/javascript-39404.png";
 
-export { RegisterBg, LoginBg };
+//icon
+import ICFacebook from "./icon/facebook.png";
+import ICTwitter from "./icon/twitter.png";
+import ICInstagram from "./icon/instagram.png";
+import ICTelegram from "./icon/telegram.png";
+import ICDiscord from "./icon/discord.png";
+import ICGithub from "./icon/github.png";
+
+export { RegisterBg, LoginBg, LogoJS, ICDiscord, ICFacebook, ICGithub, ICInstagram, ICTelegram, ICTwitter };
