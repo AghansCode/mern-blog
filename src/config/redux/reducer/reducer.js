958650890b06408644d41df79b90d2699bfe02f6
AghansCode <ganis.ade23@gmail.com>
2021-12-15 21:05:03 +0700

    refactoring create blog post

--- src/config/redux/reducer/reducer.js
+++ src/config/redux/reducer/reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import globalReducer from "./GlobalReducer";
 import homeReducer from "./HomeReducer";
+import createBlogReducer from "./createBlogReducer";
 
 //untuk menampung/mengembalikan/merubah value state
 const reducer = combineReducers({
   homeReducer,
   globalReducer,
+  createBlogReducer,
 });
 
 export default reducer;
